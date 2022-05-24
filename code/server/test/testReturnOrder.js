0379b99733a53804ea3f1430e67f386a7f10ebef
DAVIDE BOLOGNESI S295938 <s295938@studenti.polito.it>
2022-05-24 18:31:51 +0200

    fixed return order files added missing error check

--- code/server/test/testReturnOrder.js
+++ code/server/test/testReturnOrder.js
@@ -18,6 +18,13 @@ describe('test return order api', () => {
         await resDAO.deleteTableContent();
     })
 
+    restockOrderinput = {
+        "issueDate":"2021/11/29 09:33",
+        "products": [{"SKUId":12,"description":"a product","price":10.99,"qty":30},
+                    {"SKUId":180,"description":"another product","price":11.99,"qty":20}],
+        "supplierId" : 1
+    };
+
     input = {
         "returnDate":"2021/11/29 09:33",
         "products": [{"SKUId":12,"description":"a product","price":10.99,"RFID":"12345678901234567890123456789016"},
@@ -46,25 +53,39 @@ describe('test return order api', () => {
         "restockOrderId" : 1
     }
 
-    deleteReturnOrder(204, input, 1);
-    deleteReturnOrder(404, input, 2); // return order not found
-    deleteReturnOrder(422, input, -2); // invalid id
+    badInput4 = {
+        "returnDate":"2021/11/29 09:33",
+        "products": [{"SKUId":12,"description":"a product","price":10.99,"RFID":"12345678901234567890123456789016"},
+                    {"SKUId":180,"description":"another product","price":11.99,"RFID":"12345678901234567890123456789038"}],
+        "restockOrderId" : 3
+    }
+
+    deleteReturnOrder(204, input, 1 ,restockOrderinput);
+    deleteReturnOrder(404, input, 2, restockOrderinput); // return order not found
+    deleteReturnOrder(422, input, -2, restockOrderinput); // invalid id
     
-    newReturnOrder(201, input); // returnOrder created
-    newReturnOrder(422, badInput1); // issue date undefined
-    newReturnOrder(422, badInput2); // restockOrderId negative
-    newReturnOrder(422, badInput3); // SKUId negative
+    newReturnOrder(201, input, restockOrderinput); // returnOrder created
+    newReturnOrder(422, badInput1, restockOrderinput); // issue date undefined
+    newReturnOrder(422, badInput2, restockOrderinput); // restockOrderId negative
+    newReturnOrder(422, badInput3, restockOrderinput); // SKUId negative
+    newReturnOrder(404, badInput4, restockOrderinput); // restockOrder not found
     
-    getListReturnOrders(200, input); // list return orders
-    getReturnOrder(200, input, 1);
-    getReturnOrder(404, input, 3); // return order not found
-    getReturnOrder(422, input, "a"); // invalid id
+    getListReturnOrders(200, input, restockOrderinput); // list return orders
+    getReturnOrder(200, input, 1, restockOrderinput);
+    getReturnOrder(404, input, 3, restockOrderinput); // return order not found
+    getReturnOrder(422, input, "a", restockOrderinput); // invalid id
 
 });
 
-function deleteReturnOrder(expectedHTTPStatus, input, deleteId) {
+function deleteReturnOrder(expectedHTTPStatus, input, deleteId, restockOrder) {
     it('delete return order', function (done) {
-        agent.post('/api/returnOrder')
+        agent.post('/api/restockOrder')
+        .send(restockOrder)
+        .then(function (restk) {
+            restk.should.have.status(201);
+
+
+            agent.post('/api/returnOrder')
             .send(input)
             .then(function (res) {
                 res.should.have.status(201);
@@ -74,74 +95,108 @@ function deleteReturnOrder(expectedHTTPStatus, input, deleteId) {
                         done();
                     });
             });
+
+
+        }).catch(done);
     });
 }
 
-function newReturnOrder(expectedHTTPStatus, input) {
+function newReturnOrder(expectedHTTPStatus, input, restockOrder) {
     it('adding a new return order', function (done) {
-        agent.post('/api/returnOrder')
-            .send(input)
-            .then(function (res) {
-                res.should.have.status(expectedHTTPStatus);
-                if (res.status == 201) {
-                    agent.get('/api/returnOrders/1')
-                        .then(function (r) {
-                            r.should.have.status(200);
-                            r.body.id.should.equal(1);
-                            r.body.returnDate.should.equal(input.returnDate);
-                            r.body.products.length.should.equal(input.products.length);
-                            r.body.restockOrderId.should.equal(input.restockOrderId);
-                        })
-                }
-                done();
-            }).catch(done);
-    });
-}
-
-function getListReturnOrders(expectedHTTPStatus, input) {
-    it('getting return orders list', function (done) {
-        agent.post('/api/returnOrder')
-            .send(input)
-            .then(function (res) {
-                res.should.have.status(201);
-                agent.post('/api/returnOrder')
-                    .send(input)
-                    .then(function (r) {
-                        r.should.have.status(201);
-                        agent.get('/api/returnOrders')
-                            .then(function (r1) {
-                                r1.should.have.status(expectedHTTPStatus);
-                                r1.body.length.should.equal(2);
-                                done();
+        
+        agent.post('/api/restockOrder')
+        .send(restockOrder)
+        .then(function (restk) {
+            restk.should.have.status(201);
+            
+            agent.post('/api/returnOrder')
+                .send(input)
+                .then(function (res) {
+                    res.should.have.status(expectedHTTPStatus);
+                    if (res.status == 201) {
+                        agent.get('/api/returnOrders/1')
+                            .then(function (r) {
+                                r.should.have.status(200);
+                                r.body.id.should.equal(1);
+                                r.body.returnDate.should.equal(input.returnDate);
+                                r.body.products.length.should.equal(input.products.length);
+                                r.body.restockOrderId.should.equal(input.restockOrderId);
                             })
-                    });
-            });
+                    }
+                    done();
+                }).catch(done);
+
+        });
+
     });
 }
 
-function getReturnOrder(expectedHTTPStatus, input, id) {
+function getListReturnOrders(expectedHTTPStatus, input, restockOrder) {
+    it('getting return orders list', function (done) {
+
+        agent.post('/api/restockOrder')
+        .send(restockOrder)
+        .then(function (restk) {
+            restk.should.have.status(201);
+
+            agent.post('/api/returnOrder')
+                .send(input)
+                .then(function (res) {
+                    res.should.have.status(201);
+                    agent.post('/api/returnOrder')
+                        .send(input)
+                        .then(function (r) {
+                            r.should.have.status(201);
+                            agent.get('/api/returnOrders')
+                                .then(function (r1) {
+                                    r1.should.have.status(expectedHTTPStatus);
+                                    r1.body.length.should.equal(2);
+                                    done();
+                                })
+                        });
+                });
+
+
+        });
+
+    });
+}
+
+function getReturnOrder(expectedHTTPStatus, input, id, restockOrder) {
     it('getting return order', function (done) {
-        agent.post('/api/returnOrder')
-            .send(input)
-            .then(function (res) {
-                res.should.have.status(201);
-                agent.post('/api/returnOrder')
-                    .send(input)
-                    .then(function (r) {
-                        r.should.have.status(201);
-                        agent.get('/api/returnOrders/' + id)
-                            .then(function (r1) {
-                                if (r1.status == 200) {
-                                    r1.should.have.status(expectedHTTPStatus);
-                                    r1.body.returnDate.should.equal(input.returnDate);
-                                    r1.body.restockOrderId.should.equal(input.restockOrderId);
-                                    done();
-                                } else {
-                                    r1.should.have.status(expectedHTTPStatus);
-                                    done();
-                                }
-                            });
-                    });
-            });
+
+
+        agent.post('/api/restockOrder')
+        .send(restockOrder)
+        .then(function (restk) {
+            restk.should.have.status(201);
+
+            agent.post('/api/returnOrder')
+                .send(input)
+                .then(function (res) {
+                    res.should.have.status(201);
+                    agent.post('/api/returnOrder')
+                        .send(input)
+                        .then(function (r) {
+                            r.should.have.status(201);
+                            agent.get('/api/returnOrders/' + id)
+                                .then(function (r1) {
+                                    if (r1.status == 200) {
+                                        r1.should.have.status(expectedHTTPStatus);
+                                        r1.body.returnDate.should.equal(input.returnDate);
+                                        r1.body.restockOrderId.should.equal(input.restockOrderId);
+                                        done();
+                                    } else {
+                                        r1.should.have.status(expectedHTTPStatus);
+                                        done();
+                                    }
+                                });
+                        });
+                });
+
+
+        });
+
+
     });
 }
